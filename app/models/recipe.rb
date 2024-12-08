be9b1c0a5113005962c02d53c713be081d38b4de
Sachin Gandhi <sachingandhi@live.com>
2024-12-08 22:57:00 +0000

    Adds the search bar and style:
    - Creates a method on the recipe model which takes in a string of ingredients, and outputs the result of a query to check for the presence of a combination of those ingredients in a recipe
    - calls that method from the controller action, taking the param from the search bar as the argument
    - apply some style to keep my eyes from burning

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,32 @@
 class Recipe < ApplicationRecord
   validates :title, :cook_time, :prep_time, :ingredients, presence: true
   validates :title, uniqueness: true
+
+  def self.search_by_ingredients(ingredients)
+    return all if ingredients.blank?
+
+    # Split the query into individual ingredients
+    ingredients = ingredients.downcase.split
+
+    # Match recipes containing any of the ingredients
+    conditions = build_conditions(ingredients)
+    values = build_values(ingredients)
+
+    where(conditions, *values)
+  end
+
+  private
+
+  private
+
+  def self.build_conditions(ingredients)
+    # create a comparison condition and set up the query string to combine the ingredients
+    ingredients.map { "LOWER(ingredients) LIKE ?" }.join(' AND ')
+  end
+
+  def self.build_values(ingredients)
+    # produce the string of ingredients separated by spaces,
+    # to be substituted into the ? part of the query string
+    ingredients.map { |ingredient| "%#{ingredient}%" }
+  end
 end
