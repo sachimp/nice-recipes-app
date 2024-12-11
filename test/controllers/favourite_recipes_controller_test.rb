606fa9c38054122b7d950b548f4b76c8de100aca
Sachin Gandhi <sachingandhi@live.com>
2024-12-11 00:02:55 +0000

    Add some controller tests for favourite recipes, and add duplicate favourite prevention

--- test/controllers/favourite_recipes_controller_test.rb
+++ test/controllers/favourite_recipes_controller_test.rb
@@ -1,7 +1,41 @@
-require "test_helper"
+require 'test_helper'
+require 'bcrypt'
 
 class FavouriteRecipesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    @recipe = recipes(:one)
+    @favourite_recipe = favourite_recipes(:one)
+    sign_in @user
+  end
+
+  test "should create favourite recipe" do
+    @favourite_recipe.destroy!
+
+    assert_difference("FavouriteRecipe.count", 1) do
+      post favourite_recipes_path, params: { recipe_id: @recipe.id }
+    end
+
+    assert_redirected_to :pages_index
+  end
+
+  test "should not create duplicate favourite recipe" do
+    FavouriteRecipe.create(user: @user, recipe: @recipe)
+
+    assert_no_difference("FavouriteRecipe.count") do
+      post favourite_recipes_path, params: { recipe_id: @recipe.id }
+    end
+
+    assert_redirected_to :pages_index
+  end
+
+  test "should destroy favourite recipe" do
+    assert_difference("FavouriteRecipe.count", -1) do
+      delete favourite_recipe_path(@favourite_recipe)
+    end
+
+    assert_redirected_to user_path(@user)
+  end
 end
